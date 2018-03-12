8e6773c6ea29e698c001b1bff9d210f99a639e96
Unknown <i.runets95@gmail.com>
2018-03-12 21:17:04 +0300

    you-are-awesome/feature: createSerializedObject

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ const createIncrementer = () => {};
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => {
+    return Object('object');
+};
 const toBuffer = () => {};
 const sortByProto = (arr) => {
     return arr.sort(function(a,b){
